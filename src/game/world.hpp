c2e6aa20e6e34973b0f14603064a395a480aabed
Roman Dobias <me@romandobias.net>
2025-03-01 15:22:57 +0100

    feat: prototyping collision state detection
    - this is needed as later we will need to resolve the collision
      situation by alterning speed

--- src/game/world.hpp
+++ src/game/world.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <SDL.h>
+#include <array>
+#include <bitset>
+#include <cassert>
 #include <chrono>
-#include <vector>
 #include <queue>
+#include <vector>
 
 #include "application.hpp"
 
@@ -45,6 +48,98 @@ struct Ball
     ballBody.w = 2.0 * radius;
     ballBody.h = 2.0 * radius;
     return ballBody;
+  }
+
+  enum Corner
+  {
+    top_left = 0,
+    top_right = 1,
+    bottom_right = 2,
+    bottom_left = 3
+  };
+
+  std::array<SDL_FPoint, 4> getBoundingRectCorners()
+  {
+    const auto b = getBoundingRect();
+    return { SDL_FPoint{ b.x, b.y },   // top-left
+             { b.x + b.w, b.y },       // top-right
+             { b.x + b.w, b.y + b.h }, // bottom-right
+             { b.x, b.y + b.h } };     // bottom-left
+  }
+
+  enum class CollisionState
+  {
+    no_collision,
+    from_left,
+    from_right,
+    from_above,
+    from_bottom,
+    left_top_corner,
+    right_top_corner,
+    left_bottom_corner,
+    right_bottom_corner,
+    full_inside,
+  };
+
+  CollisionState getCollisionStateForGivenRect(SDL_FRect rect)
+  {
+    const auto body = getBoundingRect();
+    if (!SDL_HasIntersectionF(&rect, &body)) {
+      return CollisionState::no_collision;
+    }
+
+    const auto corners = getBoundingRectCorners();
+    std::bitset<4> isCornerInside;
+
+    for (int i = 0; i < 4; i++) {
+      isCornerInside[i] = SDL_PointInFRect(&corners[i], &rect);
+    }
+
+    const auto cornersInsideCount = isCornerInside.count();
+
+    // impossible situation for two rectangles
+    assert(cornersInsideCount != 3);
+
+    switch (cornersInsideCount) {
+      case 4:
+        return CollisionState::full_inside;
+      case 1:
+      {
+        if (isCornerInside[Corner::top_left])
+          return CollisionState::right_bottom_corner;
+        if (isCornerInside[Corner::top_right])
+          return CollisionState::left_bottom_corner;
+        if (isCornerInside[Corner::bottom_left])
+          return CollisionState::right_top_corner;
+        if (isCornerInside[Corner::bottom_right])
+          return CollisionState::left_top_corner;
+        assert(false);
+      }
+
+      case 2:
+      {
+        if (isCornerInside[Corner::top_left] &&
+            isCornerInside[Corner::top_right])
+          return CollisionState::from_bottom;
+
+        if (isCornerInside[Corner::top_left] &&
+            isCornerInside[Corner::bottom_left])
+          return CollisionState::from_right;
+
+        if (isCornerInside[Corner::top_right] &&
+            isCornerInside[Corner::bottom_right])
+          return CollisionState::from_left;
+
+        if (isCornerInside[Corner::bottom_right] &&
+            isCornerInside[Corner::bottom_left])
+          return CollisionState::from_above;
+      }
+      default:
+        assert(cornersInsideCount != 0 && cornersInsideCount != 3);
+    }
+
+    assert(false);
+    return CollisionState::no_collision;
   }
 };
 
--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -1,7 +1,58 @@
 #include <SDL.h>
+#include <cassert>
+#include <iostream>
+
+#include <game/world.hpp>
+
+void
+testCollisionStateDetection()
+{
+
+  {
+    SDL_FRect rect = { 0, 0, 100, 100 };
+
+    Ball ball;
+    ball.radius = 10;
+    ball.position = { 20, 20 };
+    assert(ball.getCollisionStateForGivenRect(rect) ==
+           Ball::CollisionState::full_inside);
+  }
+
+  {
+    SDL_FRect rect = { 0, 0, 100, 100 };
+
+    Ball ball;
+    ball.radius = 10;
+    ball.position = { 0, 0 };
+    assert(ball.getCollisionStateForGivenRect(rect) ==
+           Ball::CollisionState::left_top_corner);
+  }
+
+  {
+    SDL_FRect rect = { 0, 0, 100, 100 };
+
+    Ball ball;
+    ball.radius = 10;
+    ball.position = { 120, 120 };
+    assert(ball.getCollisionStateForGivenRect(rect) ==
+           Ball::CollisionState::no_collision);
+  }
+
+  {
+    SDL_FRect rect = { 0, 0, 100, 100 };
+
+    Ball ball;
+    ball.radius = 10;
+    ball.position = { 100, 60 };
+    assert(ball.getCollisionStateForGivenRect(rect) ==
+           Ball::CollisionState::from_right);
+  }
+}
 
 int
 main(int argc, char* args[])
 {
-    return 0;
+  testCollisionStateDetection();
+  std::cout << "end" << std::endl;
+  return 0;
 }
